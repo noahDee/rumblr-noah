36efcf5c363e0a827b6ef477f8c5e848815a5c52
noah <you@example.com>
2019-02-12 18:07:42 -0500

    styling, easy login, small fixes here and there

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,28 +4,50 @@ require 'faker'
 
 
 photos = ["dog", "cat", "bunny", "fox", "koala", "lion", "tiger", "gorilla"]
-nums = (1..6).to_a
-tags = ["bus", "cow", "horse", "dog", "pig"]
+nums = (2..4).to_a
+tags = ["bird", "cow", "horse", "dog", "pig"]
 
-
-# 30.times do
-#   User.create(
-#     name: Faker::Name.name,
-#     username: Faker::Internet.user_name,
-#     email: Faker::Internet.safe_email,
-#     birthdate: Faker::Date.birthday,
-#     password: "1234",
-#     photo: photos.sample,
-#     tags: Faker::Lorem.words(6, true).join(",")
-#   )
-# end
+User.create(
+  name: Faker::Name.name,
+  username: 'whoishe',
+  email: 'email@example.com',
+  birthdate: Faker::Date.birthday,
+  password: "1234",
+  photo: photos.sample,
+  tags: tags.join(',')
+)
 
 20.times do
   Post.create(
-    title: Faker::Lorem.sentence(4, true, 4),
+    title: Faker::Book.title,
     content: Faker::Lorem.paragraph(6, false, 6),
     # tags: Faker::Lorem.word(6, true).join(","),
     tags: tags.sample,
-    # user: nums.sample,
+    user_id: 1
+  )
+end
+
+4.times do
+  User.create(
+    name: Faker::Name.name,
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.safe_email,
+    birthdate: Faker::Date.birthday,
+    password: "1234",
+    photo: photos.sample,
+    tags: tags.sample
+  )
+end
+
+
+
+
+40.times do
+  Post.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(6, false, 6),
+    # tags: Faker::Lorem.word(6, true).join(","),
+    tags: tags.sample,
+    user_id: nums.sample,
 )
 end
